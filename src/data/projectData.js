00214a54863bdfd3ece5198c5303440eafc77fc0
Shaun Niel Ochavo <shaunniel02@gmail.com>
2023-07-06 10:09:14 +0800

    Add temporary datas

--- src/data/projectData.js
+++ src/data/projectData.js
@@ -8,6 +8,44 @@ export default [
       "https://upload.wikimedia.org/wikipedia/commons/1/16/Appearance_of_sky_for_weather_forecast%2C_Dhaka%2C_Bangladesh.JPG",
     ],
     stack: ["HTML", "CSS", "JavaScript", "Java", "SCSS", "Python"],
-    link: "https://github.com/shaunniekins",
+    demoLink: "https://github.com/shaunniekins",
+    repoLink: "https://github.com/shaunniekins",
+  },
+  {
+    title: "Terminal Website",
+    description: "Description of Terminal Website",
+    imgUrls: [
+      "https://images.unsplash.com/photo-1664575196079-9ac04582854b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80",
+      "https://img.freepik.com/free-photo/white-cloud-blue-sky_74190-7709.jpg?w=2000",
+      "https://upload.wikimedia.org/wikipedia/commons/1/16/Appearance_of_sky_for_weather_forecast%2C_Dhaka%2C_Bangladesh.JPG",
+    ],
+    stack: ["HTML", "CSS", "JavaScript", "Java"],
+    demoLink: "https://github.com/shaunniekins",
+    repoLink: "https://github.com/shaunniekins",
+  },
+
+  {
+    title: "Terminal Website",
+    description: "Description of Terminal Website",
+    imgUrls: [
+      "https://images.unsplash.com/photo-1664575196079-9ac04582854b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80",
+      "https://img.freepik.com/free-photo/white-cloud-blue-sky_74190-7709.jpg?w=2000",
+      "https://upload.wikimedia.org/wikipedia/commons/1/16/Appearance_of_sky_for_weather_forecast%2C_Dhaka%2C_Bangladesh.JPG",
+    ],
+    stack: ["HTML", "CSS", "JavaScript", "Java"],
+    demoLink: "https://github.com/shaunniekins",
+    repoLink: "https://github.com/shaunniekins",
+  },
+  {
+    title: "Terminal Website",
+    description: "Description of Terminal Website",
+    imgUrls: [
+      "https://images.unsplash.com/photo-1664575196079-9ac04582854b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80",
+      "https://img.freepik.com/free-photo/white-cloud-blue-sky_74190-7709.jpg?w=2000",
+      "https://upload.wikimedia.org/wikipedia/commons/1/16/Appearance_of_sky_for_weather_forecast%2C_Dhaka%2C_Bangladesh.JPG",
+    ],
+    stack: ["HTML", "CSS", "JavaScript", "Java"],
+    demoLink: "https://github.com/shaunniekins",
+    repoLink: "https://github.com/shaunniekins",
   },
 ];
